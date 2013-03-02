4450049be1a9e14c8897040be9b465c4bc39f441
张 贝贝 <keepyouliking@gmail.com>
2013-03-02 09:18:09 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yingjie::Application.config.secret_token = '5b7a7b49ca9eb4d86a38f78405780a7d10606112f2655951ab059c0f8a88eaea9cbef66644c60bd00be26805d0fc7f442a877f7358929b0c930e0257ae626a50'
