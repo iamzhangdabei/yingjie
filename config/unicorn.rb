eee69f0fd5bc5e030d5a2e5dd29eb2588fb1f71a
张 贝贝 <keepyouliking@gmail.com>
2013-03-02 16:19:27 +0800

    unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,7 +3,7 @@
 # ------------------------------------------------------------------------------
 
 # Set your full path to application.
-app_path=File.dirname(File.dirname(File.dirname(File.expand_path(__FILE__))))
+app_path="/u/apps/yingjie"
 rails_env = ENV["RAILS_ENV"] || "production"
 
 # Set unicorn options
